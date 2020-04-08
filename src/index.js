ad2f47cd1f76f9c2791d78167d371a967c2bfeff
BilalHussain009 <bilalhussain.work@gmail.com>
2020-04-08 16:41:21 +0500

    CORS

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ require('./db/mongoose')
 
 const userRouter=require('./routers/user')
 const postRouter=require('./routers/post')
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+       next();
+ });
 app.use(express.json())
 const port=process.env.PORT
 app.use(userRouter,postRouter)
+
 app.listen(port,()=>{
     console.log("Server up on "+port)
 })
